7930e8fbd5dbc67e78c123dacda2473dc9e5a408
xpartla <xpartla@stuba.sk>
2025-04-19 19:21:08 +0200

    get support, refactoring

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use std::fs;
 use std::io::Cursor;
 use boa_engine::object::FunctionObjectBuilder;
 
+const JSFILE: &str = "./logic/bundle.js";
+
 fn main() {
     let server = Server::http("0.0.0.0:8000").unwrap();
     println!("Running on http://0.0.0.0:8000");
@@ -31,7 +33,7 @@ fn main() {
                 request.respond(res).unwrap();
                 continue;
             }
-            let js_result = process_post_with_js(&path, &body);
+            let js_result = process_get_with_js(&path, &body);
             let res = create_json_response(js_result);
             request.respond(res).unwrap();
         } else {
@@ -42,38 +44,52 @@ fn main() {
 }
 
 fn create_bad_request_response() -> Response<Cursor<Vec<u8>>> {
-    let res = Response::from_string("{\"error\": \"Bad Request\"}")
+    Response::from_string("{\"error\": \"Bad Request\"}")
         .with_status_code(400)
         .with_header(Header {
             field: "Content-Type".parse().unwrap(),
             value: "application/json".parse().unwrap(),
-        });
-    return res;
+        })
 }
 
 fn create_not_found_response() -> Response<Cursor<Vec<u8>>> {
-    let res = Response::from_string("{\"error\": \"Not Found\"}")
+    Response::from_string("{\"error\": \"Not Found\"}")
         .with_status_code(404)
         .with_header(Header {
             field: "Content-Type".parse().unwrap(),
             value: "application/json".parse().unwrap(),
-        });
-    return res;
+        })
 }
 
 fn create_json_response(js_result: String) -> Response<Cursor<Vec<u8>>> {
-    let res = Response::from_string(js_result)
+    Response::from_string(js_result)
         .with_header(Header {
             field: "Content-Type".parse().unwrap(),
             value: "application/json".parse().unwrap()
-        });
-    return res;
+        })
 }
 
 fn process_post_with_js(path: &str, payload_json: &str) -> String {
     let mut context = Context::default();
     inject_console(&mut context);
-    let file = "./logic/bundle.js";
+    let file = JSFILE;
+    let js_code = fs::read_to_string(file).unwrap_or_else(|_| {
+        panic!("Unable to read JS file: {}", file);
+    });
+
+    context
+        .eval(Source::from_bytes(js_code.as_bytes()))
+        .expect(&format!("JS eval failed for {}", file));
+    
+    let js_call = format_js_for_post(path, payload_json);
+    
+    match_js_string(&mut context, js_call)
+}
+
+fn process_get_with_js(path: &str, payload_json: &str) -> String {
+    let mut context = Context::default();
+    inject_console(&mut context);
+    let file = JSFILE;
     let js_code = fs::read_to_string(file).unwrap_or_else(|_| {
         panic!("Unable to read JS file: {}", file);
     });
@@ -82,8 +98,13 @@ fn process_post_with_js(path: &str, payload_json: &str) -> String {
         .eval(Source::from_bytes(js_code.as_bytes()))
         .expect(&format!("JS eval failed for {}", file));
 
+    let js_call = format_js_for_get(path, payload_json);
 
-    let js_call = format!(
+    match_js_string(&mut context, js_call)
+}
+
+fn format_js_for_post(path :&str, payload_json :&str) -> String {
+    format!(
         r#"
         (function() {{
             let req = {{
@@ -98,21 +119,35 @@ fn process_post_with_js(path: &str, payload_json: &str) -> String {
     "#,
         path = path,
         body = payload_json
-    );
+    )
+}
 
+fn format_js_for_get(path :&str, payload_json :&str) -> String {
+    format!(
+        r#"
+        (function() {{
+            let req = {{
+                method: "GET",
+                path: "{path}",
+                body: {body}
+            }};
+            let res = {{}};
+            dispatch(req, res);
+            return JSON.stringify(res);
+        }})()
+    "#,
+        path = path,
+        body = payload_json
+    )
+}
 
+fn match_js_string(context: &mut Context, js_call: String) -> String {
     match context.eval(Source::from_bytes(js_call.as_bytes())) {
-        Ok(val) => match val.to_string(&mut context) {
+        Ok(val) => match val.to_string(context) {
             Ok(js_str) => js_str.to_std_string().unwrap_or_else(|_| "{\"error\": \"Invalid JS string\"}".to_string()),
             Err(_) => "{\"error\": \"No output\"}".to_string(),
         },
-        Err(err) => {
-            let err_msg = format!(
-                "{{\"error\": \"JS Error: {}\"}}",
-                err.to_string()
-            );
-            err_msg
-        }
+        Err(err) => format!("{{\"error\": \"JS Error: {}\"}}", err.to_string()),
     }
 }
 
@@ -167,7 +202,3 @@ fn inject_console(context: &mut Context) {
         .register_global_property(JsString::from("console"), console, Attribute::all())
         .unwrap();
 }
-
-// fn process_get_with_js(path: &str, payload: &str) -> String {
-//
-// }
